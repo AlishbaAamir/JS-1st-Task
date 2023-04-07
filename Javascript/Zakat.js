4853a27dab42c524eaf914a15fb2c35ddc14a2a9
AlishbaAamir <126007592+AlishbaAamir@users.noreply.github.com>
2023-04-07 20:14:05 +0500

    calculate-zakat

---
+++ Javascript/Zakat.js
@@ -0,0 +1,6 @@
+// Task 1      (6-April-2023)
+var zakatpercentage=0.025;
+// Take input from uer
+var input=+prompt("Calculate your Zakat amount");
+var result= zakatpercentage*input;
+alert("Zakt is " +result);
